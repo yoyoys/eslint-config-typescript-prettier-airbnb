4d5e7bf23fe53a144ba607668dc987f98b019e28
Yoyo Young <yoyoys@livemail.tw>
2020-05-07 14:18:11 +0800

    feat: add eslint rule

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ module.exports = {
     'airbnb-typescript/base',
     'prettier',
     'prettier/@typescript-eslint',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   plugins: ['eslint-plugin-prettier'],
   rules: {
@@ -20,7 +21,6 @@ module.exports = {
       trailingComma: 'all',
     }],
     'no-useless-constructor': 'off',
-    '@typescript-eslint/no-useless-constructor': 'error',
     // provide by prettier
     'semi': 'off',
     'arrow-parens': 'off',
@@ -28,8 +28,28 @@ module.exports = {
     'object-curly-newline': 'off',
     'comma-dangle': 'off',
     'implicit-arrow-linebreak': 'off',
-    'indent': 0,
-    'space-before-blocks': 0,
+    'indent': 'off',
+    'space-before-blocks': 'off',
+
+    // additional config
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/array-type': ['error', { default: 'array' }],
+    '@typescript-eslint/explicit-member-accessibility': ['warn', { accessibility: 'explicit' }],
+    '@typescript-eslint/no-extra-non-null-assertion': ['error'],
+    '@typescript-eslint/no-extraneous-class': ['error', { allowWithDecorator: true }],
+    '@typescript-eslint/promise-function-async': ['error'],
+    '@typescript-eslint/require-array-sort-compare': ['warn'],
+    '@typescript-eslint/restrict-plus-operands': ['error'],
+    '@typescript-eslint/restrict-template-expressions': ['error', { allowNumber: true }],
+    '@typescript-eslint/switch-exhaustiveness-check': ['warn'],
+    '@typescript-eslint/no-unnecessary-condition': ['error', { ignoreRhs: true }],
+    '@typescript-eslint/no-floating-promises': ['error'],
+    '@typescript-eslint/class-name-casing': ['error'],
+
+    // not ideal for prototype
+    '@typescript-eslint/no-explicit-any': ['off'],
+    'max-classes-per-file': 'off',
+
   },
   parserOptions: {
     sourceType: 'module',
@@ -38,5 +58,8 @@ module.exports = {
     "import/resolver": {
       "typescript": {},
     },
+  },
+  parserOptions: {
+    project: ['./tsconfig.json'],
   },
 };
